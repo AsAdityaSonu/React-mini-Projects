0498cca9b931915a21d4dd66b391727e6e9ce21d
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-03-29 14:23:57 +0530

    TailWind Props

---
+++ 02_TailWind_Props/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl">hi</h1>
+
+      <Card />
+    </>
+  );
+}
+
+export default App;
