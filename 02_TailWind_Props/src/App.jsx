d8b378fd11e17d69abf510119324b58060795006
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-03-29 14:25:01 +0530

    TailWind Props done :)

--- 02_TailWind_Props/src/App.jsx
+++ 02_TailWind_Props/src/App.jsx
@@ -2,11 +2,18 @@ import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+  let myObj = {
+    username:"Aditya",
+    age:20
+  }
+
+  let newArray = [1,2,34,45,65]
+
   return (
     <>
       <h1 className="bg-green-400 text-black p-4 rounded-xl">hi</h1>
 
-      <Card />
+      <Card channel="Aditya" someObj = {myObj} arr = {newArray} />
     </>
   );
 }
