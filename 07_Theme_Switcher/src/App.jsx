2850bfde59da3f330c6d950540db60b3c8052e05
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-06-09 15:48:24 +0530

    context API basic template done and project init

---
+++ 07_Theme_Switcher/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+
+    return (
+        <div>
+            <p>React and context API</p>
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ 07_Theme_Switcher/src/context/userContextProvider.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import UserContext from "./userContext";
+
+const UserContextProvider = (props) => {};
+
+export default UserContextProvider;
